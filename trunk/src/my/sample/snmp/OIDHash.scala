13443c4f994b712b145201dd12454b46d2b4baee
murali.venugopal <murali.venugopal@c05bc3a0-7560-8fbb-c127-d7c356b97012>
2013-03-14 11:02:47 +0000

    chumma

--- trunk/src/my/sample/snmp/OIDHash.scala
+++ trunk/src/my/sample/snmp/OIDHash.scala
@@ -6,7 +6,8 @@ object OIDHash {
 
   def main(args: Array[String]): Unit = {
     
-    val oid:OID = new OID(args(0))
+//    val oid:OID = new OID(args(0))
+    val oid:OID = new OID("1.3.6.1.2.1.25.2.3.1.5.35")
     println(oid.hashCode())
     
   }
